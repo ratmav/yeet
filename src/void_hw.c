4936bc174a4833812284e6ed0e15056902a30428
Chris Cuming <ratmav@protonmail.com>
2021-01-26 04:04:34 -0500

    wip: stdlib isn't available in kernel module source, so emitting a message on a timer will be trickier than i thought. i read up a little on linux workqueues, it seems like a good place to investigate.

--- src/void_hw.c
+++ src/void_hw.c
@@ -1,8 +1,17 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/workqueue.h>
 
 MODULE_LICENSE("GPL");
+
+void workqueue_fn(struct work_struct *work);
+
+DECLARE_WORK(workqueue,workqueue_fn);
+
+void workqueue_fn(struct work_struct *work) {
+        printk(KERN_INFO "YEET\n");
+}
 
 static int __init yeet_init(void) {
  printk(KERN_INFO "preparing to yeet.\n");
