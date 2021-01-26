83f68410f3dd43c455de8595272ae457b2a3e416
Chris Cuming <ratmav@protonmail.com>
2021-01-25 23:26:30 -0500

    mfw copypasta includes 'special' quote characters.

--- src/void_hw.c
+++ src/void_hw.c
@@ -2,16 +2,16 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-MODULE_LICENSE(“GPL”);
+MODULE_LICENSE("GPL");
 
-static int __init lkm_example_init(void) {
- printk(KERN_INFO “preparing to yeet.\n”);
+static int __init yeet_init(void) {
+ printk(KERN_INFO "preparing to yeet.\n");
  return 0;
 }
 
-static void __exit lkm_example_exit(void) {
- printk(KERN_INFO “yeeting complete.\n”);
+static void __exit yeet_exit(void) {
+ printk(KERN_INFO "yeeting complete.\n");
 }
 
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+module_init(yeet_init);
+module_exit(yeet_exit);
